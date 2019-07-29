365aa3418e139aeae5cfb4cdc21865d0c3e5e6ec
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-29 15:47:17 +0200

    Configure project to use clarifai

--- server.js
+++ server.js
@@ -12,25 +12,36 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host: 'ec2-54-243-142-182.compute-1.amazonaws.com',
+    user: 'lgqxugxlyrlldm',
+    password: 'd2e0213631661cb1b6bc26370cc834cbfe7aba0b4347f52acf0e2f7f8c5c947b',
+    database: 'daigod15kmgf8t',
+    ssl: true
   }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+  res.send(db.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
-})
+});
