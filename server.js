ed7f71d0d2978746d57bb25c62e7b076fa8a07c9
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-08-29 09:59:25 +0200

    Add auth middleware

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
 console.log(process.env.POSTGRES_HOST);
 console.log(process.env.POSTGRES_USER);
@@ -37,16 +38,16 @@ app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
-app.get('/profile/:id', (req, res) => {
+app.get('/profile/:id', auth.requireAuth, (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
-app.post('/profile/:id', (req, res) => {
+app.post('/profile/:id', auth.requireAuth, (req, res) => {
   profile.handleProfileUpdate(req, res, db);
 });
-app.put('/image', (req, res) => {
+app.put('/image', auth.requireAuth, (req, res) => {
   image.handleImage(req, res, db);
 });
-app.post('/imageurl', (req, res) => {
+app.post('/imageurl', auth.requireAuth, (req, res) => {
   image.handleApiCall(req, res);
 });
 
